326b6bb1093be0d6ec94053bb24ff24c6ca80e0a
Navaneeth Sai Adapa <84430609+Navaneethadapa@users.noreply.github.com>
2023-03-25 08:14:37 +0530

    Add files via upload

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,32 @@
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+  {
+    label: 'Articles',
+    pathname: '/articles',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/Navaneeth_adapa',
+    icon: 'twitter',
+  },
+  
+  {
+    label: 'Github',
+    url: 'https://github.com/Navaneethadapa',
+    icon: 'github',
+  },
+];
